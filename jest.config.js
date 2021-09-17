2e5f0efa5584901c831b32abeac59161ec247e81
Fajar Septiawan <fajar.septiawan@zebrax.id>
2021-09-17 12:23:53 +0700

    first commit

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|js?|tsx?|ts?)$',
+}
---
+++ tests/App.test.tsx
@@ -0,0 +1,16 @@
+/**
+ * @format
+ */
+
+import React from 'react'
+import 'react-native'
+
+import renderer from 'react-test-renderer'
+
+import App from '../App'
+
+// Note: test renderer must be required after react-native.
+
+it('renders correctly', () => {
+  renderer.create(<App />)
+})
